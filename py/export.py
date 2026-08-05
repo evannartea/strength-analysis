cd12f9aabbd0c9309add847f0f4d884082c945ee
Evan Nartea <evannartea@Mac.lan>
2026-08-05 15:16:59 +0100

    added print statement

--- py/export.py
+++ py/export.py
@@ -8,3 +8,7 @@ def export_to_csv(table_name, engine):
     df.to_csv(file_path, index=False)
 
     return file_path
+
+export_to_csv("openpowerlifting_20260801", engine)
+
+print("Exported successfully!")
