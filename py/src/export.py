971fb7019bb0794aeeed8b89f7c7eaf2153916e0
Evan Nartea <evannartea@Mac.lan>
2026-08-05 17:47:19 +0100

    move folders

---
+++ py/src/export.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from py.src.config import engine
+
+def export_to_csv(table_name, engine):
+    df = pd.read_sql(f"SELECT * FROM staging.{table_name}", engine)
+
+    file_path = f"data/clean/{table_name}.csv"
+    df.to_csv(file_path, index=False)
+
+    return file_path
+
+export_to_csv("openpowerlifting_20260801", engine)
+
+print("Exported successfully!")
