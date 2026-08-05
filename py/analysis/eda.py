7d8ffee49ddb9313e81f54f7681da24b7af0654a
Evan Nartea <evannartea@Mac.lan>
2026-08-05 20:16:40 +0100

    drop place field from summary

--- py/analysis/eda.py
+++ py/analysis/eda.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 df = pd.read_csv("data/clean/openpowerlifting_20260801.csv")
 
-male_lifters = df[df["Sex"] == "M"]
-female_lifters = df[df["Sex"] == "F"]
+male_lifters = df[df["Sex"] == "M"].drop(columns=["Place"])
+female_lifters = df[df["Sex"] == "F"].drop(columns=["Place"])
 
 summary_m = male_lifters.describe().round(1)
 print(f"{tabulate(summary_m, headers='keys', tablefmt='github')}\n")
