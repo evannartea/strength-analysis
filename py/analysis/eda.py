28c5fb25e6137ae78ea88bf39b216f202c41fa38
Evan Nartea <evannartea@Mac.lan>
2026-08-05 17:47:41 +0100

    add new folder

---
+++ py/analysis/eda.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv("data/clean/openpowerlifting_20260801.csv")
+
+print(df.describe().round(1))
+print(df.isnull().sum())
+print(df.select_dtypes(include="number").corr())
