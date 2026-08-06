eeb1b9f58bb6e6c3704a38962ead730fc4d9cc86
Evan Nartea <evannartea@Mac.lan>
2026-08-06 13:32:29 +0100

    added comments

--- py/analysis/eda.py
+++ py/analysis/eda.py
@@ -3,14 +3,16 @@ import pandas as pd
 
 df = pd.read_csv("data/clean/openpowerlifting_20260801.csv")
 
+# Male lifters summary
 male_lifters = df[df["Sex"] == "M"].drop(columns=["Place"])
-female_lifters = df[df["Sex"] == "F"].drop(columns=["Place"])
-
 summary_m = male_lifters.describe().round(1)
 print(f"{tabulate(summary_m, headers='keys', tablefmt='github')}\n")
 
+# Female lifters summary
+female_lifters = df[df["Sex"] == "F"].drop(columns=["Place"])
 summary_f = female_lifters.describe().round(1)
 print(f"{tabulate(summary_f, headers='keys', tablefmt='github')}\n")
 
+# Null count
 null_count = df.isnull().sum().reset_index()
 print(tabulate(null_count, headers=["Column", "Null Count"], tablefmt="github", showindex=False))
