bc78d042a6f3875b6329179fd20facf698acfea8
Evan Nartea <evannartea@Mac.lan>
2026-08-06 17:15:25 +0100

    add age analysis

---
+++ py/analysis/age_analysis.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data/clean/openpowerlifting_20260801.csv")
+
+# Set groups
+male_lifters = df[df["Sex"] == "M"]
+female_lifters = df[df["Sex"] == "F"]
+
+# Scatter plot
+plt.scatter(
+    male_lifters["Age"],
+    male_lifters["TotalKg"],
+    label="Male",
+    color="skyblue",
+    alpha=0.5,
+    s=20
+)
+
+plt.scatter(
+    female_lifters["Age"],
+    female_lifters["TotalKg"],
+    label="Female",
+    color="lightcoral",
+    alpha=0.5,
+    s=20
+)
+
+plt.xlabel("Age")
+plt.ylabel("Total Kg")
+plt.legend(title="Sex")
+
+plt.tight_layout()
+plt.show()
