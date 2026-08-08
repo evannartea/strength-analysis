2e35f28c5bae70df91622c59d6aaf66024d49559
Evan Nartea <evannartea@Mac.lan>
2026-08-08 14:59:26 +0100

    added country analysis

--- py/analysis/country_analysis.py
+++ py/analysis/country_analysis.py
@@ -6,8 +6,12 @@ df = pd.read_csv("data/clean/openpowerlifting_20260801.csv")
 
 # Total by Country
 def total_country_m_bar_chart(df):
-    male_lifters = df[df["Sex"] == "M"]
-    avg_squat_m = (
+    df_filtered = df[(df["Sex"] == "M") & (df["AgeClass"].isin(["24-34", "35-39"]))]
+    male_lifters = df_filtered[
+        df_filtered.groupby("Country")["Country"].transform("size") > 1000
+    ]
+
+    avg_total_m = (
         male_lifters
         .groupby("Country")["TotalKg"]
         .mean()
@@ -16,20 +20,70 @@ def total_country_m_bar_chart(df):
     )
 
     plt.figure(figsize=(12,6))
-    
+
     # Bar chart
-    plt.bar(
-       avg_squat_m.index,
-       avg_squat_m.values
+    bars = plt.bar(
+       avg_total_m.index,
+       avg_total_m.values,
+       color="skyblue"
     )
 
+    plt.bar_label(
+        bars,
+        fmt="%.1f",
+        fontsize=8,
+        padding=3
+    )
+
+    plt.xticks(rotation=45)
     plt.xlabel("Country")
     plt.ylabel("Average Total (kg)")
     plt.tight_layout()
 
-    #plt.savefig("figures/squat_by_country.png", dpi=300, bbox_inches="tight")
-    #print("Figure saved successfully!")
-    #plt.close()
-    plt.show()
+    plt.savefig("figures/total_by_country_m.png", dpi=300, bbox_inches="tight")
+    print("Figure saved successfully!")
+    plt.close()
+    #plt.show()
+
+def total_country_f_bar_chart(df):
+    df_filtered = df[(df["Sex"] == "F") & (df["AgeClass"].isin(["24-34", "35-39"]))]
+    female_lifters = df_filtered[
+        df_filtered.groupby("Country")["Country"].transform("size") > 1000
+    ]
+
+    avg_total_f = (
+        female_lifters
+        .groupby("Country")["TotalKg"]
+        .mean()
+        .sort_values(ascending=False)
+        .head(20)
+    )
+
+    plt.figure(figsize=(12,6))
+
+    # Bar chart
+    bars = plt.bar(
+       avg_total_f.index,
+       avg_total_f.values,
+       color="lightcoral"
+    )
+
+    plt.bar_label(
+        bars,
+        fmt="%.1f",
+        fontsize=8,
+        padding=3
+    )
+
+    plt.xticks(rotation=45)
+    plt.xlabel("Country")
+    plt.ylabel("Average Total (kg)")
+    plt.tight_layout()
+
+    plt.savefig("figures/total_by_country_f.png", dpi=300, bbox_inches="tight")
+    print("Figure saved successfully!")
+    plt.close()
+    #plt.show()
 
 total_country_m_bar_chart(df)
+total_country_f_bar_chart(df)
